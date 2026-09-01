a8dbf57ba364550ead4aba3a68d075b725c1d98b
Lee <karasactualemail@gmail.com>
2026-09-01 17:22:09 -0600

    forgot to run format oops

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage('so-cart') || [];
+  const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -16,5 +16,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
