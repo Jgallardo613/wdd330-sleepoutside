30fe593ee8579755a696e69af3658b6dd593ccf1
benblazer13 <benblazer2020@gmail.com>
2026-09-08 21:06:42 -0400

    cart can remove items, cart cant see cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,30 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
+  const cartItems = getCartItems();
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  document.querySelector('.product-list').innerHTML =
+    htmlItems.join('') || '<li class="cart-empty">Your cart is empty.</li>';
+}
+
+function getCartItems() {
+  try {
+    const cartItems = getLocalStorage('so-cart');
+    return Array.isArray(cartItems) ? cartItems : [];
+  } catch {
+    return [];
+  }
+}
+
+function removeCartItem(productId) {
+  const updatedCart = getCartItems().filter((item) => item.Id !== productId);
+  setLocalStorage('so-cart', updatedCart);
+  renderCartContents();
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <button class="cart-card__remove" type="button" data-id="${item.Id}" aria-label="Remove ${item.Name} from cart">X</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -24,5 +41,12 @@ function cartItemTemplate(item) {
 
   return newItem;
 }
+
+document.querySelector('.product-list').addEventListener('click', (event) => {
+  const removeButton = event.target.closest('.cart-card__remove');
+  if (removeButton) {
+    removeCartItem(removeButton.dataset.id);
+  }
+});
 
 renderCartContents();
