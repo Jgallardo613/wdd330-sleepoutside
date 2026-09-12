f2d471a874f922ccf9e3672987a425f68db882ea
Lee <karasactualemail@gmail.com>
2026-09-12 14:51:30 -0600

    attempt failed

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+console.log("Main script loaded");
+
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import "./newsletter.js";
 
 const productList = new ProductList("tents", new ProductData(), document.getElementById("product-list"));
 const productData = new ProductData();
---
+++ src/js/newsletter.js
@@ -0,0 +1,20 @@
+console.log("Newsletter script loaded");
+
+const newsDialouge = document.getElementById("newsletter-dialouge");
+const openNews = document.getElementById("openNews");
+const closeNews = document.getElementById("closeNews");
+const subNews = document.getElementById("subscribeNews");
+
+openNews.addEventListener("click", () => {
+    newsDialouge.showModal();
+});
+
+closeNews.addEventListener("click", () => {
+    newsDialouge.close();
+});
+
+subNews.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert('Thank you for subscribing!');
+    newsDialouge.close();
+});
